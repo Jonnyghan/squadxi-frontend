5f72416148ea091ee26dd1f92ee512b1f4bc113f
Jonathan <Jonnyghan@gmail.com>
2020-11-02 16:02:03 -0500

    Installed packages and imported to index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
 class App extends React.Component {
- render(){
+ 
+ componentDidMount(){
+   fetch('http://localhost:3000/api/v1/teams', {
+     method: 'GET'
+   })
+   .then(response => response.json())
+   .then(teams => console.log(teams))
+ }
+ 
+  render(){
   return (
     <div className="App">
       App
